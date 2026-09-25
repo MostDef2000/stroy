d16a3dce4722c7a19807b8c67777b2642c5991e7
Most Def <289312555+MostDef2000@users.noreply.github.com>
2026-09-25 14:20:12 +1000

    feat: add style profile package

---
+++ src/stroy/style/__init__.py
@@ -0,0 +1,17 @@
+from stroy.style.models import (
+    LightingStyle,
+    MaterialStyle,
+    PaletteEntry,
+    StyleProfile,
+    StyleProfileProposal,
+    merge_style_overrides,
+)
+
+__all__ = [
+    "LightingStyle",
+    "MaterialStyle",
+    "PaletteEntry",
+    "StyleProfile",
+    "StyleProfileProposal",
+    "merge_style_overrides",
+]
